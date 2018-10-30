b6e69b987606a7d96b1f6dcbd0608d9ea5712c2c
Fabien RAHAINGOMANANA <fabrahaingo@hotmail.fr>
2018-10-30 03:53:18 +0100

    modified main file

--- getMeATicket.js
+++ getMeATicket.js
@@ -1,6 +1,7 @@
 const puppeteer = require('puppeteer');
 const prompt = require("prompt-async");
 
+// For testing purposes
 function delay(timeout) {
   return new Promise((resolve) => {
     setTimeout(resolve, timeout);
@@ -9,14 +10,13 @@ function delay(timeout) {
 
 (async () => {
   const browser = await puppeteer.launch({
-    headless: false
+    headless: false // Don't set to true (or else you won't see the final booking page)
   })
   const page = await browser.newPage();
 
-  // First goes to login page to identify
+  // Go to login page
   await page.goto('https://billetterie.operadeparis.fr/account/login')
 
-  // Get all needed values from login page
   const USERNAME_SELECTOR = '#login';
   const PASSWORD_SELECTOR = '#password';
   const BUTTON_SELECTOR = '#continue_button > span.text';
@@ -27,16 +27,17 @@ function delay(timeout) {
 			  required: true
 		  },
 		  password: {
-			  hidden: true,
+			  hidden: true, // So people won't see you type your password
 			  required: true
 		  }
 	  }
   }
 
+  // Asking for user credentials
   prompt.start();
   const credentials = await prompt.get(schema);
 
-  // Input credentials, then log in
+  // Inputing credentials
   await page.click(USERNAME_SELECTOR);
   await page.keyboard.type(credentials.username);
   await page.click(PASSWORD_SELECTOR);
@@ -44,17 +45,19 @@ function delay(timeout) {
   await page.click(BUTTON_SELECTOR);
   await page.waitForNavigation();
 
-  // EITHER successful login OR returns error
+  // Returns error if login timed out
   try {
   	await page.waitForSelector('body > div.content-wrapper > section > div > div > div > h2');
   }
   catch (error) {
-	console.log("Couldn't connect with those credentials...");
+	console.log("Couldn't log you in...");
 	await page.close();
 	await browser.close();
 	return;
   }
 
+  // Asking for BALLET or OPERA
+  // Asking for PERF_NAME (replace all ' ' by '-') => "la dame aux camelias" must become "la-dame-aux-camelias"
   prompt.start();
   const url_values = await prompt.get(["performance_type", "performance_name"]);
   const target_url = 'https://www.operadeparis.fr/saison-18-19/' + url_values.performance_type + '/' + url_values.performance_name + '/performances';
@@ -63,11 +66,13 @@ function delay(timeout) {
       {waitUntil: 'load'});
 
   var iterations = 1;
-  
+
+  // Getting JSON content  
   innerText = await page.evaluate(() => {
 	return JSON.parse(document.querySelector("body").innerText); 
   });
 
+  // Making AJAX requests until product gets available
   while (innerText.items[0].template !== "available") {
   	await page.goto(target_url,
 		{waitUntil: 'load'});
@@ -75,21 +80,24 @@ function delay(timeout) {
 	innerText = await page.evaluate(() => {
 		return JSON.parse(document.querySelector("body").innerText);
 	});
+	// Displaying number of refresh iterations
 	if (iterations % 5 == 0) {
 		console.log('page refreshed ' + iterations + ' times');
 	}
 	iterations++;
   }
 
+  // If product gets available, finds link and redirects to it
   if (innerText.items[0].template === "available") {
 	const found_url = innerText.items[0].content.expand.blocks[0][0].buttons[1].url;
 	console.log(found_url);
 	console.log("Found a reservation link on the page, getting you there");
 
 	await page.goto(found_url,
-		{waitUntil: 'networkidle0'});
+		{waitUntil: 'networkidle0'});	
 	return;
   }
+  // In case product is available but not link was provided...
   else {
 	console.log("No reservation link available yet...");
   	await browser.close()
